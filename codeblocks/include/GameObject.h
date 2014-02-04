60f8eeb475689c22b79fc418891105a9850a8a81
SuperKaitoKid <starwarstheforceunleshed@gmail.com>
2014-02-04 14:34:50 -0800

    Updated GameObject template.
    Conflicts:
    	codeblocks/Brave Soul.depend
    	codeblocks/include/GameObject.h
    	codeblocks/src/gameObject.cpp

--- codeblocks/include/GameObject.h
+++ codeblocks/include/GameObject.h
@@ -1,19 +1,13 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
-<<<<<<< HEAD:codeblocks/include/GameObject.h
 #include <vector>
 #include "AnimatedSprite.h"
 #include "Attack.h"
 
 class GameObject{
-=======
->>>>>>> parent of e6eb7aa... Added game logic framework.:codeblocks/include/gameObject.h
 
-class gameObject
-{
 public:
-<<<<<<< HEAD:codeblocks/include/GameObject.h
     GameObject();
     ~GameObject();
 
@@ -26,12 +20,6 @@ private:
     AnimatedSprite* spr;
     std::vector<enum_effect>effects;
 
-=======
-    gameObject();
-    ~gameObject();
-protected:
-private:
->>>>>>> parent of e6eb7aa... Added game logic framework.:codeblocks/include/gameObject.h
 };
 
 #endif // GAMEOBJECT_H
